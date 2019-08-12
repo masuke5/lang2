5608eb880ff38a103fa65ae5bbbbbac2af6c6779
masuke5 <s.zerogoichi@gmail.com>
2019-08-12 10:04:13 +0900

    Implement Error trait

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::error;
+use std::fmt;
 use crate::span::Span;
 
 #[derive(Debug, PartialEq)]
@@ -12,5 +14,21 @@ impl Error {
             msg: msg.to_string(),
             span,
         }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} at {}:{}-{}:{}", self.msg,
+               self.span.start_line,
+               self.span.start_col,
+               self.span.end_line,
+               self.span.end_col)
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        "Parse error"
     }
 }
