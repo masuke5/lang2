8525bf92a69d3478110c4b1b84776d99d360cee7
masuke5 <s.zerogoichi@gmail.com>
2020-11-07 14:33:19 +0900

    Add build function

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,15 +1,30 @@
 use crate::ast::SymbolPath;
-use crate::id::Id;
+use crate::id::{Id, IdMap};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use opcode::Opcode;
 use rustc_hash::FxHashMap;
 use std::fmt;
+use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use std::str;
 use std::sync::atomic::{AtomicU32, Ordering};
 use thiserror::Error;
 
 #[allow(dead_code)]
 pub mod opcode {
     #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
-    pub struct Opcode(u8);
+    pub struct Opcode(pub u8);
+
+    impl Opcode {
+        #[inline]
+        pub fn is_extended(self) -> bool {
+            (self.0 >> 7) != 0
+        }
+
+        #[inline]
+        pub fn extended(self) -> Self {
+            Self(self.0 | (1 << 7))
+        }
+    }
 
     pub const NOP: Opcode = Opcode(0x1);
     pub const INT: Opcode = Opcode(0x2);
@@ -70,13 +85,15 @@ pub mod opcode {
     pub const BINOP_AND: Opcode = Opcode(0x37);
     pub const BINOP_OR: Opcode = Opcode(0x38);
     pub const BINOP_XOR: Opcode = Opcode(0x39);
-    pub const CALL: Opcode = Opcode(0x3a);
-    pub const CALL_REF: Opcode = Opcode(0x3b);
-    pub const CALL_NATIVE: Opcode = Opcode(0x3c);
-    pub const RETURN: Opcode = Opcode(0x3d);
-    pub const JUMP: Opcode = Opcode(0x3e);
-    pub const JUMP_IF_TRUE: Opcode = Opcode(0x3f);
-    pub const JUMP_IF_FALSE: Opcode = Opcode(0x40);
+    pub const MODULE: Opcode = Opcode(0x3b);
+    pub const FUNC: Opcode = Opcode(0x3c);
+    pub const SELF_FUNC: Opcode = Opcode(0x3d);
+    pub const CALL_REF: Opcode = Opcode(0x3e);
+    pub const CALL_NATIVE: Opcode = Opcode(0x3f);
+    pub const RETURN: Opcode = Opcode(0x40);
+    pub const JUMP: Opcode = Opcode(0x41);
+    pub const JUMP_IF_TRUE: Opcode = Opcode(0x42);
+    pub const JUMP_IF_FALSE: Opcode = Opcode(0x43);
 }
 
 const MAX_ARG: u32 = 0b1111_1111_1111_1111_1111_1111;
@@ -116,6 +133,7 @@ pub enum BuilderError {
 
 #[derive(Debug, Clone, PartialEq)]
 enum Inst {
+    Function(SymbolPath, Id),
     Normal(Opcode, u32),
     Jump(Opcode, Label),
 }
@@ -125,14 +143,16 @@ pub struct Function {
     pub escaped_stack_size: u32,
     pub stack_size: u32,
     labels: FxHashMap<Label, usize>,
+    param_count: u32,
     insts: Vec<Inst>,
 }
 
 impl Function {
-    pub fn new() -> Self {
+    pub fn new(param_count: u32) -> Self {
         Self {
             escaped_stack_size: 0,
             stack_size: 0,
+            param_count,
             insts: Vec::new(),
             labels: FxHashMap::default(),
         }
@@ -187,9 +207,10 @@ impl Function {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct BytecodeBuilder {
     module_path: SymbolPath,
+    imported_modules: Vec<SymbolPath>,
     integers: Vec<u64>,
     floats: Vec<f64>,
     strings: Vec<String>,
@@ -200,6 +221,7 @@ impl BytecodeBuilder {
     pub fn new(module_path: &SymbolPath) -> Self {
         Self {
             module_path: module_path.clone(),
+            imported_modules: Vec::new(),
             integers: Vec::new(),
             floats: Vec::new(),
             strings: Vec::new(),
@@ -243,11 +265,352 @@ impl BytecodeBuilder {
         Ok((self.strings.len() - 1) as u32)
     }
 
+    pub fn define_function(&mut self, name: Id, param_count: u32) -> Result<(), BuilderError> {
+        if self.functions.len() >= MAX_ARG as usize {
+            return Err(BuilderError::TooMany("too many functions", MAX_ARG as u64));
+        }
+        if param_count >= MAX_ARG {
+            return Err(BuilderError::TooMany("too many parameters", MAX_ARG as u64));
+        }
+
+        self.functions.insert(name, Function::new(param_count));
+        Ok(())
+    }
+
+    pub fn import_module(&mut self, path: &SymbolPath) -> Result<(), BuilderError> {
+        if self.imported_modules.len() >= MAX_ARG as usize {
+            return Err(BuilderError::TooMany(
+                "too many imported modules",
+                MAX_ARG as u64,
+            ));
+        }
+
+        self.imported_modules.push(path.clone());
+        Ok(())
+    }
+
     pub fn get_func(&self, name: Id) -> &Function {
         self.functions.get(&name).unwrap()
     }
 
     pub fn get_func_mut(&mut self, name: Id) -> &mut Function {
         self.functions.get_mut(&name).unwrap()
+    }
+
+    fn allocate_ids_for_functions(&self) -> FxHashMap<Id, u32> {
+        let mut function_ids = FxHashMap::default();
+        let mut next_id = 0;
+
+        for (name, _) in &self.functions {
+            function_ids.insert(*name, next_id);
+            next_id += 1;
+        }
+
+        function_ids
+    }
+
+    fn build(self, function_ids: &FxHashMap<SymbolPath, FxHashMap<Id, u32>>) -> Vec<u8> {
+        // Allocate IDs for modules
+        let modules = {
+            let mut next_id = 0;
+            let mut modules = FxHashMap::default();
+            for module in &self.imported_modules {
+                modules.insert(module.clone(), next_id);
+                next_id += 1;
+            }
+            modules
+        };
+
+        let module_names = self
+            .imported_modules
+            .iter()
+            .map(|path| (format!("{}", path), modules[path] as usize))
+            .collect();
+
+        // Create functions
+        let mut functions = Vec::with_capacity(self.functions.len());
+        for (name, function) in &self.functions {
+            functions.push(BytecodeFunction {
+                id: function_ids[&self.module_path][&name] as usize,
+                name: IdMap::name(*name),
+                stack_size: function.stack_size as usize,
+                escaped_stack_size: function.escaped_stack_size as usize,
+                param_count: function.param_count as usize,
+                pos: 0,
+            });
+        }
+
+        let mut function_insts = FxHashMap::default();
+
+        for (name, function) in self.functions {
+            fn push_u32(insts: &mut Vec<(opcode::Opcode, u8)>, op: opcode::Opcode, arg: u32) {
+                assert!(arg <= MAX_ARG);
+
+                if arg > 0xff {
+                    insts.push((op.extended(), (arg >> 16) as u8));
+                    insts.push((opcode::Opcode((arg >> 8) as u8), arg as u8));
+                } else {
+                    insts.push((op, arg as u8));
+                }
+            }
+
+            fn push_i32(insts: &mut Vec<(opcode::Opcode, u8)>, op: opcode::Opcode, arg: i32) {
+                push_u32(insts, op, u32::from_le_bytes(arg.to_le_bytes()));
+            }
+
+            let mut insts = Vec::with_capacity(function.insts.len());
+            for (i, inst) in function.insts.into_iter().enumerate() {
+                match inst {
+                    Inst::Function(module_path, name) => {
+                        push_u32(&mut insts, opcode::MODULE, modules[&module_path]);
+                        push_u32(&mut insts, opcode::FUNC, function_ids[&module_path][&name])
+                    }
+                    Inst::Jump(op, label) => {
+                        push_i32(&mut insts, op, i as i32 - function.labels[&label] as i32);
+                    }
+                    Inst::Normal(op, arg) => {
+                        push_u32(&mut insts, op, arg);
+                    }
+                }
+            }
+
+            function_insts.insert(function_ids[&self.module_path][&name], insts);
+        }
+
+        let module = BytecodeModule {
+            path: format!("{}", self.module_path),
+            modules: module_names,
+            functions,
+            integers: self.integers,
+            floats: self.floats,
+            strings: self.strings,
+        };
+
+        module.to_bytes(function_insts)
+    }
+}
+
+pub fn build_bytecode(
+    builders_per_module: FxHashMap<SymbolPath, BytecodeBuilder>,
+) -> FxHashMap<String, Vec<u8>> {
+    // Allocate IDs for functions in each modules
+    let mut function_ids = FxHashMap::default();
+    for (module_path, builder) in &builders_per_module {
+        function_ids.insert(module_path.clone(), builder.allocate_ids_for_functions());
+    }
+
+    builders_per_module
+        .into_iter()
+        .map(|(path, builder)| (format!("{}", path), builder.build(&function_ids)))
+        .collect()
+}
+
+#[derive(Debug, Clone)]
+pub struct BytecodeFunction {
+    pub id: usize,
+    pub name: String,
+    pub stack_size: usize,
+    pub escaped_stack_size: usize,
+    pub param_count: usize,
+    pub pos: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct BytecodeModule {
+    pub path: String,
+    pub modules: Vec<(String, usize)>,
+    pub functions: Vec<BytecodeFunction>,
+    pub integers: Vec<u64>,
+    pub floats: Vec<f64>,
+    pub strings: Vec<String>,
+}
+
+impl BytecodeModule {
+    pub fn from_bytes(bytes: Vec<u8>, path: &str) -> Option<Self> {
+        type LE = LittleEndian;
+
+        fn align(bytes: &mut Cursor<Vec<u8>>) {
+            bytes.set_position(bytes.position() + (8 - bytes.position() % 8));
+        }
+
+        let mut bc = Cursor::new(bytes);
+
+        // Check header
+        let mut buf = [0u8; 4];
+        bc.read_exact(&mut buf).ok()?;
+        if &buf != b"L201" {
+            return None;
+        }
+
+        align(&mut bc);
+
+        // Read modules
+        let modules_count = bc.read_u64::<LE>().ok()?;
+        let mut modules = Vec::with_capacity(modules_count as usize);
+        for _ in 0..modules_count {
+            let id = bc.read_u32::<LE>().ok()?;
+
+            // Read module path
+            let len = bc.read_u32::<LE>().ok()? as usize;
+            let mut buf = vec![0; len];
+            bc.read_exact(&mut buf).ok()?;
+            let path = str::from_utf8(&buf).ok()?;
+            align(&mut bc);
+
+            modules.push((path.to_string(), id as usize));
+        }
+
+        // Read functions
+        let func_count = bc.read_u64::<LE>().ok()?;
+        let mut functions = Vec::with_capacity(func_count as usize);
+        for _ in 0..func_count {
+            let id = bc.read_u32::<LE>().ok()?;
+
+            // Read function name
+            let len = bc.read_u32::<LE>().ok()? as usize;
+            let mut buf = vec![0; len];
+            bc.read_exact(&mut buf).ok()?;
+            let name = str::from_utf8(&buf).ok()?.to_string();
+            align(&mut bc);
+
+            let stack_size = bc.read_u32::<LE>().ok()?;
+            let escaped_stack_size = bc.read_u32::<LE>().ok()?;
+            let param_count = bc.read_u32::<LE>().ok()?;
+            let pos = bc.read_u64::<LE>().ok()?;
+            align(&mut bc);
+
+            let func = BytecodeFunction {
+                id: id as usize,
+                name,
+                stack_size: stack_size as usize,
+                escaped_stack_size: escaped_stack_size as usize,
+                param_count: param_count as usize,
+                pos: pos as usize,
+            };
+            functions.push(func);
+        }
+
+        // Read integers
+        let integer_count = bc.read_u32::<LE>().ok()?;
+        align(&mut bc);
+        let mut integers = Vec::with_capacity(integer_count as usize);
+        for _ in 0..integer_count {
+            let n = bc.read_u64::<LE>().ok()?;
+            integers.push(n);
+        }
+
+        // Read floats
+        let float_count = bc.read_u32::<LE>().ok()?;
+        align(&mut bc);
+        let mut floats = Vec::with_capacity(float_count as usize);
+        for _ in 0..float_count {
+            let n = bc.read_f64::<LE>().ok()?;
+            floats.push(n);
+        }
+
+        // Read strings
+        let string_count = bc.read_u32::<LE>().ok()?;
+        align(&mut bc);
+        let mut strings = Vec::with_capacity(string_count as usize);
+        for _ in 0..string_count {
+            let len = bc.read_u64::<LE>().ok()? as usize;
+            let mut buf = vec![0; len];
+            bc.read_exact(&mut buf).ok()?;
+            let s = str::from_utf8(&buf).ok()?.to_string();
+            align(&mut bc);
+
+            strings.push(s);
+        }
+
+        Some(Self {
+            path: path.to_string(),
+            modules,
+            functions,
+            integers,
+            floats,
+            strings,
+        })
+    }
+
+    fn to_bytes(&self, insts: FxHashMap<u32, Vec<(opcode::Opcode, u8)>>) -> Vec<u8> {
+        type LE = LittleEndian;
+
+        fn align(bytes: &mut Cursor<Vec<u8>>) {
+            for _ in 0..8 - bytes.position() % 8 {
+                bytes.write_all(&[0]).unwrap();
+            }
+        }
+
+        let mut bc = Cursor::new(Vec::new());
+        bc.write_all(b"L201").unwrap();
+        align(&mut bc);
+
+        // Write modules
+        bc.write_u64::<LE>(self.modules.len() as u64).unwrap();
+        for (path, id) in &self.modules {
+            bc.write_u32::<LE>(*id as u32).unwrap();
+            bc.write_u32::<LE>(path.len() as u32).unwrap();
+            bc.write_all(path.as_bytes()).unwrap();
+            align(&mut bc);
+        }
+
+        // Write functions
+        let mut func_pos = FxHashMap::<u32, u64>::default();
+        bc.write_u64::<LE>(self.functions.len() as u64).unwrap();
+        for func in &self.functions {
+            bc.write_u32::<LE>(func.id as u32).unwrap();
+            bc.write_u32::<LE>(func.name.len() as u32).unwrap();
+            bc.write_all(func.name.as_bytes()).unwrap();
+            align(&mut bc);
+
+            bc.write_u32::<LE>(func.stack_size as u32).unwrap();
+            bc.write_u32::<LE>(func.escaped_stack_size as u32).unwrap();
+            bc.write_u32::<LE>(func.param_count as u32).unwrap();
+            func_pos.insert(func.id as u32, bc.position());
+            bc.write_u64::<LE>(!0).unwrap();
+            align(&mut bc);
+        }
+
+        // Write integers
+        bc.write_u32::<LE>(self.integers.len() as u32).unwrap();
+        align(&mut bc);
+        for n in &self.integers {
+            bc.write_u64::<LE>(*n).unwrap();
+        }
+
+        // Write floats
+        bc.write_u32::<LE>(self.floats.len() as u32).unwrap();
+        align(&mut bc);
+        for n in &self.floats {
+            bc.write_f64::<LE>(*n).unwrap();
+        }
+
+        // Write strings
+        bc.write_u32::<LE>(self.strings.len() as u32).unwrap();
+        align(&mut bc);
+        for s in &self.strings {
+            bc.write_u64::<LE>(s.len() as u64).unwrap();
+            bc.write_all(s.as_bytes()).unwrap();
+            align(&mut bc);
+        }
+
+        // Write instructions
+        for (id, insts) in insts {
+            // Write pos
+            bc.seek(SeekFrom::Start(func_pos[&id])).unwrap();
+            bc.write_u64::<LE>(bc.position()).unwrap();
+
+            // Write instructions
+            bc.seek(SeekFrom::End(0)).unwrap();
+            bc.write_u64::<LE>(insts.len() as u64).unwrap();
+            for (op, arg) in insts {
+                bc.write_u8(op.0).unwrap();
+                bc.write_u8(arg).unwrap();
+            }
+
+            align(&mut bc);
+        }
+
+        bc.into_inner()
     }
 }
